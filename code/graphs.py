c39b2b9015aa1ea1aa81f626cb04136fdd1770ad
Viki Nemecek <ryba@divocaci.cz>
2021-12-27 07:40:30 +0100

    Add more data&graphs

--- code/graphs.py
+++ code/graphs.py
@@ -20,7 +20,7 @@ COLORS = {
     'tango': 'tab:red'
 }
 
-def load_data(path):
+def load_data(path, modified=False):
     line_data = None
     data = []
     for i, line in enumerate(open('../data/'+path, 'r')):
@@ -32,7 +32,7 @@ def load_data(path):
                 'tree_size': int(line[1]),
                 'sequence_length': int(line[2]),
                 'sequence_type': line[4],
-                'subset_size': None if line[4] in 'sr' else line[5],
+                'subset_size': None if line[4] in 'sri' else line[5],
                 }
         else:
             line = line.split()
@@ -40,6 +40,13 @@ def load_data(path):
             data.append(line_data)
 
     df = pd.DataFrame(data)
+
+    if modified is not None:
+        if modified:
+            df = df.loc[(df['structure'] != 'rb') | (df['value_type'] != 'time')].reset_index(drop=True).copy()
+            df.loc[df['structure'] == 'rbrandom', 'structure'] = 'rb'
+        else:
+            df = df.loc[df['structure'] != 'rbrandom'].reset_index(drop=True).copy()
 
     return df
 
@@ -83,11 +90,12 @@ def by_one_seq_graphs(prints=False):
         plt.grid()
         plt.legend()
         plt.savefig('../text/graphs/one_by_one_seq.pdf')
+        plt.close()
         plt.figure()
 
 
-def simple_graphs(seq_type, prints=False):
-    df = load_data('data')
+def simple_graphs(seq_type, prints=False, modified=True):
+    df = load_data('data', modified=modified)
     df['value'] /= df['sequence_length']
     df = df.loc[df['sequence_type'] == seq_type].copy()
 
@@ -132,6 +140,7 @@ def simple_graphs(seq_type, prints=False):
         plt.legend()
         plt.grid()
         plt.savefig(f'../text/graphs/{type_}_{seq_type}.pdf')
+        plt.close()
         plt.figure()
 
 
@@ -141,7 +150,7 @@ def randomized_graphs():
     df1 = df.loc[(df['sequence_type'] == 'r') & (df['structure'] == 'rb')].copy()
     df3 = df.loc[(df['sequence_type'] == 'r') & (df['structure'] == 'splay')].copy()
 
-    df2 = load_data('data_random')
+    df2 = load_data('data_random', modified=None)
     df2['value'] /= df2['sequence_length']
     #  df2['structure'] = 'rbrandom'
 
@@ -162,6 +171,7 @@ def randomized_graphs():
     plt.legend()
     plt.grid()
     plt.savefig(f'../text/graphs/randomized_rb.pdf')
+    plt.close()
     plt.figure()
 
 
@@ -182,14 +192,51 @@ def variance_random_tango_touch():
     plt.legend()
     plt.grid()
     plt.savefig(f'../text/graphs/variance_tango_random.pdf')
+    plt.close()
     plt.figure()
+
+
+def per_tree_subset(seq_type):
+    df = load_data('data')
+    df['value'] /= df['sequence_length']
+    secondary_type = 'r' if seq_type=='u' else 's'
+    df = df.loc[((df['sequence_type'] == seq_type) | (df['sequence_type'] == secondary_type)) & (df['tree_size'] <= 1e7)].copy().fillna(0)
+
+    for type_ in ['touch', 'time']:
+
+        df_ = df.loc[df['value_type'] == type_].copy()
+
+        for st, aux in df_.groupby('structure'):
+
+            label = LABELS[type_]
+            plt.semilogx()
+
+            color = ([COLORS[st]]*4 + ['gray']*4).__iter__()
+            styles = (['-', ':', '--', '-.']*2).__iter__()
+
+            for subset_size, aux_ in aux.groupby('subset_size'):
+                plt.plot(aux_['tree_size'], aux_['value'], label=LABELS[st]+(', '+str(subset_size) if subset_size else ''), color=next(color), linestyle=next(styles))
+
+            plt.xlabel('Počet vrcholů stromu')
+            plt.ylabel(label)
+            plt.legend()
+            plt.grid()
+            plt.savefig(f'../text/graphs/{st}_{type_}_{seq_type}.pdf')
+            plt.close()
+            plt.figure()
+
 
 
 
 if __name__ == '__main__':
     by_one_seq_graphs()
     simple_graphs('s', True)
-    simple_graphs('r')
+    simple_graphs('r', modified=False)
+    simple_graphs('i')
+
     variance_random_tango_touch()
     randomized_graphs()
+
+    per_tree_subset('u')
+    per_tree_subset('b')
 
